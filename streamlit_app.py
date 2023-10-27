3eeb035e4b76ae269ec5947077c6e77cd1e61c9c
RinkeshMore <129475029+RinkeshMore@users.noreply.github.com>
2023-10-27 16:57:52 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,8 +21,8 @@ streamlit.dataframe(fruits_to_show)
 
 def get_fruityvice_data(this_fruit_choice):
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice) 
-        fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-        return fruityvice_normalized
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
 
 streamlit.header("Fruityvice Fruit Advice!")
 try:
@@ -49,8 +49,8 @@ if streamlit.button('Get Fruit Load List'):
 
 def insert_row_snowflake(new_fruit):
     with my_cnx_cursor() as my_cur:
-        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-        return "Thanks for adding "+ new_fruit
+         my_cur.execute("insert into fruit_load_list values ('jackfruit'), ('papaya'), ('guava') , ('kiwi')")
+         return "Thanks for adding "+ new_fruit
         
 add_my_fruit = streamlit.text_input('what fruit would you like to add?')
 if stream.button('Add a fruit to the list'):
